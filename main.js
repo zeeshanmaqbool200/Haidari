6ff5d629a9850cb97bbe8b8353f1e289f5327474
zeeshanmaqbool200 <zeeshanmaqbool200@gmail.com>
2024-02-12 12:21:19 +0530

    update

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error);
+      });
+  }
 document.addEventListener("DOMContentLoaded", function() {
     var bookWrap = document.getElementById("book-wrap");
     var haidariSchoolContent = document.getElementById("haidarischool");
